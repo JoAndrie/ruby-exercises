472fa86427aa53611adb0e83f56ceb3ce51e060e
Jonathan Andrie <fabijonathanandrie@gmail.com>
2025-04-24 17:01:35 +0800

    Activity ruby 1

--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -25,7 +25,7 @@ end
 
 def remove_last_element(array)
   # Step 1: remove the last element from the array
-  array.last(1).shift(1)
+  array.pop
 
   # Step 2: return the array (because Step 1 returns the value of the element removed)
   array
@@ -41,28 +41,35 @@ end
 
 def array_concatenation(original, additional)
   # return an array adding the original and additional array together
+  original.concat(additional)
 end
 
 def array_difference(original, comparison)
   # return an array of elements from the original array that are not in the comparison array
+  original.difference(comparison)
 end
 
 def empty_array?(array)
   # return true if the array is empty
+  array.empty?
 end
 
 def reverse(array)
   # return the reverse of the array
+  array.reverse
 end
 
 def array_length(array)
   # return the length of the array
+  array.length
 end
 
 def include?(array, value)
   # return true if the array includes the value
+  array.include?(value)
 end
 
 def join(array, separator)
   # return the result of joining the array with the separator
+  array.join(separator)
 end
